0c202bf3d7c2454bde57b2d97e4182045c01f6c9
Luiz cezar <luizcezar223@gmail.com>
2024-11-16 11:11:44 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+ 
+<?php
+
+    include_once("templates/topo.php");
+    include_once("templates/menu.php");
+
+    if(empty($_SERVER['QUERY_STRING'])){
+        $var = "conteudo.php";
+        include_once($var);
+    }else{
+        $pg = $_GET['pg'];
+        include_once("$pg.php");
+    }
+
+    include_once("templates/rodape.php");
+
+?>
+
+</body>
+</html>
---
+++ quemsomos.php
@@ -0,0 +1,6 @@
+<?php
+
+    echo "<h3> Quem somos </h3>";
+    echo "<p> Somos... kajsdakjsa akkajjka jakka </p>";
+
+    
